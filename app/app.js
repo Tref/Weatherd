11fd39df12665ad41233cf6cba4efc789c4ffbf8
Tref <nick@treflabs.com>
2016-02-06 00:56:29 -0500

    listing all cities with data hardcoded on home page

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var weatheredApp = angular.module('myApp', [
   'ui.bootstrap',
   'ngRoute',
   'myApp.view1',
@@ -11,3 +11,19 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+
+weatheredApp.controller('CityListCtrl', function ($scope) {
+  $scope.cities = [
+    {'name': 'New York', 'high': 73, 'low': 54, 'pressure': 120, 'humidity': 80},
+    {'name': 'Chicago', 'high': 73, 'low': 54, 'pressure': 120, 'humidity': 80},
+    {'name': 'Seattle', 'high': 73, 'low': 54, 'pressure': 120, 'humidity': 80},
+    {'name': 'Houston', 'high': 73, 'low': 54, 'pressure': 120, 'humidity': 80},
+    {'name': 'San Diego', 'high': 73, 'low': 54, 'pressure': 120, 'humidity': 80}
+  ];
+});
+
+
+{
+
+}
