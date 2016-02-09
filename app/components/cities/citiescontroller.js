87d2b3d45333d6d8d3c5520c80ef26a32bea8e36
Tref <nick@treflabs.com>
2016-02-08 22:43:57 -0500

    adding date to weather data and style to home page

--- app/components/cities/citiescontroller.js
+++ app/components/cities/citiescontroller.js
@@ -20,12 +20,160 @@ angular.module('weatheredApp.citiesController', [])
 
   $scope.orderProp = 'name';
 
+  $scope.date = Date.now();
+
   $scope.change = function() {
-    console.log("changed to " + $scope.selectedCity.id);
-    $http.get('http://api.openweathermap.org/data/2.5/weather?id=' + $scope.selectedCity.id + '&units=imperial&appid=0e2637c065115c67e1528e72cbbfd9cb').success(function(data) {
-      console.log(data);
-      $scope.selectedCity = data;
+    // console.log("changed to " + $scope.selectedCity.id);
+
+    var cityID = $scope.selectedCity.id;
+    var currentURL = "http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=0e2637c065115c67e1528e72cbbfd9cb&id=";
+    $http.get(currentURL + cityID ).success(function(data) {
+      // console.log(data);
+      $scope.selectedCity.weatherData = data;
     });
+
+    var forecastURL = "http://api.openweathermap.org/data/2.5/forecast/daily?units=imperial&cnt=7&appid=0e2637c065115c67e1528e72cbbfd9cb&id=";
+    $http.get(forecastURL + cityID).success(function(data) {
+      // console.log(data);
+      $scope.selectedCity.forecastData = data;
+
+      // var kvArray = [{key:1, value:10}, {key:2, value:20}, {key:3, value: 30}];
+      $scope.selectedCity.graphData = $scope.selectedCity.forecastData.list.map(function(obj){
+         var dataRow = {};
+
+         var utcSeconds = obj.dt;
+         var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+         d.setUTCSeconds(utcSeconds);
+        //  var localDate = d.toLocaleDateString();
+
+         dataRow.date = d;
+         dataRow.temp = obj.temp.day;
+         return dataRow;
+      });
+
+      d3.select("svg").remove();
+
+      var graphData = $scope.selectedCity.graphData;
+
+      // DO SOME D3 Work
+      // Set the dimensions of the canvas / graph
+      var margin = {top: 10, right: 10, bottom: 20, left: 30},
+          width = 513 - margin.left - margin.right,
+          height = 270 - margin.top - margin.bottom;
+
+      // Parse the date / time
+      var parseDate = d3.time.format("%d-%b-%y").parse;
+
+      // Set the ranges
+      var x = d3.time.scale().range([0, width]);
+      var y = d3.scale.linear().range([height, 0]);
+
+      // Define the axes
+      var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5);
+      var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
+
+      // Define the line
+      var valueline = d3.svg.line()
+          .x(function(d) { return x(d.date); })
+          .y(function(d) { return y(d.temp); })
+          .interpolate("basis");
+
+      // Adds the svg canvas
+      var svg = d3.select("#chart")
+          .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+              .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+
+        x.domain(d3.extent($scope.selectedCity.graphData, function(d) { return d.date; }));
+        y.domain([
+          d3.min($scope.selectedCity.graphData, function(d) { return d.temp; }) - 20,
+          d3.max($scope.selectedCity.graphData, function(d) { return d.temp; }) + 10
+          ]);
+        // y.domain([0, 100]);
+
+        // Add the valueline path.
+        svg.append("path")
+            .attr("class", "line")
+            .attr("d", valueline($scope.selectedCity.graphData));
+
+        // Add the X Axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .append("text")
+              .attr("x", 432)
+              .attr("dx", ".71em")
+              .style("text-anchor", "end")
+              .text("Date");
+;;
+
+        // Add the Y Axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Temp (°F)");
+;
+
+    // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
+    $scope.selectedCity.id = cityID;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   };
 
 }]);
